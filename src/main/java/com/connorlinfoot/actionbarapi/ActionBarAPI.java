1cb74412b2898f3eb2a451617121b169acf453b9
Connor Linfoot <connorlinfoot@me.com>
2015-12-23 21:50:41 +0000

    Converted to Maven and added ActionBarMessageEvent

--- src/main/java/com/connorlinfoot/actionbarapi/ActionBarAPI.java
+++ src/main/java/com/connorlinfoot/actionbarapi/ActionBarAPI.java
@@ -13,116 +13,93 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 
-public class ActionBarAPI extends JavaPlugin
-{
+public class ActionBarAPI extends JavaPlugin {
     public static Plugin plugin;
     public static boolean works = true;
     public static String nmsver;
 
-    public void onEnable()
-    {
+    public void onEnable() {
         plugin = this;
         getConfig().options().copyDefaults(true);
         saveConfig();
         Server server = getServer();
         ConsoleCommandSender console = server.getConsoleSender();
-        
+
         nmsver = Bukkit.getServer().getClass().getPackage().getName();
         nmsver = nmsver.substring(nmsver.lastIndexOf(".") + 1);
 
-        console.sendMessage("");
-        console.sendMessage(ChatColor.BLUE + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-        console.sendMessage("");
-        console.sendMessage(ChatColor.AQUA + getDescription().getName());
-        console.sendMessage(ChatColor.AQUA + "Version " + getDescription().getVersion());
-        console.sendMessage("");
-        console.sendMessage(ChatColor.BLUE + "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-        console.sendMessage("");
+        console.sendMessage(ChatColor.AQUA + getDescription().getName() + " V" + getDescription().getVersion() + " has been enabled!");
     }
 
-    public void onDisable()
-    {
-        getLogger().info(getDescription().getName() + " has been disabled!");
-    }
+    public static void sendActionBar(Player player, String message) {
+        // Call the event, if cancelled don't send Action Bar
+        ActionBarMessageEvent actionBarMessageEvent = new ActionBarMessageEvent(player, message);
+        Bukkit.getPluginManager().callEvent(actionBarMessageEvent);
+        if (actionBarMessageEvent.isCancelled())
+            return;
 
-    public static void sendActionBar(Player player, String message)
-    {
-        try
-        {
+        try {
             Class<?> c1 = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
             Object p = c1.cast(player);
-            Object ppoc = null;
+            Object ppoc;
             Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
             Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + ".Packet");
-            if (nmsver.equalsIgnoreCase("v1_8_R1") || !nmsver.startsWith("v1_8_"))
-            {
+            if (nmsver.equalsIgnoreCase("v1_8_R1") || !nmsver.startsWith("v1_8_")) {
                 Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatSerializer");
                 Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-                Method m3 = c2.getDeclaredMethod("a", new Class<?>[] {String.class});
+                Method m3 = c2.getDeclaredMethod("a", String.class);
                 Object cbc = c3.cast(m3.invoke(c2, "{\"text\": \"" + message + "\"}"));
-                ppoc = c4.getConstructor(new Class<?>[] {c3, byte.class}).newInstance(new Object[] {cbc, (byte) 2});
-            } else
-            {
+                ppoc = c4.getConstructor(new Class<?>[]{c3, byte.class}).newInstance(cbc, (byte) 2);
+            } else {
                 Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
                 Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-                Object o = c2.getConstructor(new Class<?>[] {String.class}).newInstance(new Object[] {message});
-                ppoc = c4.getConstructor(new Class<?>[] {c3, byte.class}).newInstance(new Object[] {o, (byte) 2});
+                Object o = c2.getConstructor(new Class<?>[]{String.class}).newInstance(message);
+                ppoc = c4.getConstructor(new Class<?>[]{c3, byte.class}).newInstance(o, (byte) 2);
             }
-            Method m1 = c1.getDeclaredMethod("getHandle", new Class<?>[] {});
+            Method m1 = c1.getDeclaredMethod("getHandle");
             Object h = m1.invoke(p);
             Field f1 = h.getClass().getDeclaredField("playerConnection");
             Object pc = f1.get(h);
-            Method m5 = pc.getClass().getDeclaredMethod("sendPacket", new Class<?>[] {c5});
+            Method m5 = pc.getClass().getDeclaredMethod("sendPacket", c5);
             m5.invoke(pc, ppoc);
-        } catch (Exception ex)
-        {
+        } catch (Exception ex) {
             ex.printStackTrace();
             works = false;
         }
     }
 
-    public static void sendActionBar(final Player player, final String message, int duration)
-    {
+    public static void sendActionBar(final Player player, final String message, int duration) {
         sendActionBar(player, message);
 
-        if(duration>=0)
-        {
+        if (duration >= 0) {
             // Sends empty message at the end of the duration. Allows messages shorter than 3 seconds, ensures precision.
-            new BukkitRunnable()
-            {
+            new BukkitRunnable() {
                 @Override
-                public void run()
-                {
+                public void run() {
                     sendActionBar(player, "");
                 }
             }.runTaskLater(plugin, duration + 1);
         }
 
         // Re-sends the messages every 3 seconds so it doesn't go away from the player's screen.
-        while (duration > 60)
-        {
+        while (duration > 60) {
             duration -= 60;
             int sched = duration % 60;
-            new BukkitRunnable()
-            {
+            new BukkitRunnable() {
                 @Override
-                public void run()
-                {
+                public void run() {
                     sendActionBar(player, message);
                 }
             }.runTaskLater(plugin, (long) sched);
         }
     }
 
-    public static void sendActionBarToAllPlayers(String message)
-    {
+    public static void sendActionBarToAllPlayers(String message) {
         sendActionBarToAllPlayers(message, -1);
     }
 
-    public static void sendActionBarToAllPlayers(String message, int duration)
-    {
-        for(Player p : Bukkit.getOnlinePlayers())
-        {
+    public static void sendActionBarToAllPlayers(String message, int duration) {
+        for (Player p : Bukkit.getOnlinePlayers()) {
             sendActionBar(p, message, duration);
         }
     }
---
+++ src/main/java/com/connorlinfoot/actionbarapi/ActionBarMessageEvent.java
@@ -0,0 +1,48 @@
+package com.connorlinfoot.actionbarapi;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class ActionBarMessageEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+    private final Player player;
+    private String message;
+    private boolean cancelled = false;
+
+    public ActionBarMessageEvent(Player player, String message) {
+        this.player = player;
+        this.message = message;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+}
+
