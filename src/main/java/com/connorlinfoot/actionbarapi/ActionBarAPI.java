30f338881dde0c462371c30800721fd8a4a80f1c
Connor Linfoot <connorlinfoot@me.com>
2017-06-11 20:26:53 +0100

    Bukkit 1.12 support

--- src/main/java/com/connorlinfoot/actionbarapi/ActionBarAPI.java
+++ src/main/java/com/connorlinfoot/actionbarapi/ActionBarAPI.java
@@ -15,102 +15,151 @@ import java.lang.reflect.Method;
 
 
 public class ActionBarAPI extends JavaPlugin implements Listener {
-	public static Plugin plugin;
-	public static boolean works = true;
-	public static String nmsver;
-	private static boolean useOldMethods = false;
+    public static Plugin plugin;
+    public static boolean works = true;
+    public static String nmsver;
+    private static boolean useOldMethods = false;
 
-	public void onEnable() {
-		plugin = this;
-		getConfig().options().copyDefaults(true);
-		saveConfig();
+    public void onEnable() {
+        plugin = this;
+        getConfig().options().copyDefaults(true);
+        saveConfig();
 
-		CLUpdate clUpdate = new CLUpdate(this);
+        CLUpdate clUpdate = new CLUpdate(this);
 
-		Server server = getServer();
-		ConsoleCommandSender console = server.getConsoleSender();
+        Server server = getServer();
+        ConsoleCommandSender console = server.getConsoleSender();
 
-		nmsver = Bukkit.getServer().getClass().getPackage().getName();
-		nmsver = nmsver.substring(nmsver.lastIndexOf(".") + 1);
+        nmsver = Bukkit.getServer().getClass().getPackage().getName();
+        nmsver = nmsver.substring(nmsver.lastIndexOf(".") + 1);
 
-		if (nmsver.equalsIgnoreCase("v1_8_R1") || nmsver.equalsIgnoreCase("v1_7_")) { // Not sure if 1_7 works for the protocol hack?
-			useOldMethods = true;
-		}
+        if (nmsver.equalsIgnoreCase("v1_8_R1") || nmsver.startsWith("v1_7_")) { // Not sure if 1_7 works for the protocol hack?
+            useOldMethods = true;
+        }
 
-		console.sendMessage(ChatColor.AQUA + getDescription().getName() + " V" + getDescription().getVersion() + " has been enabled!");
-		Bukkit.getPluginManager().registerEvents(clUpdate, this);
-	}
+        console.sendMessage(ChatColor.AQUA + getDescription().getName() + " V" + getDescription().getVersion() + " has been enabled!");
+        Bukkit.getPluginManager().registerEvents(clUpdate, this);
+    }
 
-	public static void sendActionBar(Player player, String message) {
-		// Call the event, if cancelled don't send Action Bar
-		ActionBarMessageEvent actionBarMessageEvent = new ActionBarMessageEvent(player, message);
-		Bukkit.getPluginManager().callEvent(actionBarMessageEvent);
-		if (actionBarMessageEvent.isCancelled())
-			return;
+    public static void sendActionBar(Player player, String message) {
+        if (!player.isOnline()) {
+            return; // Player may have logged out
+        }
+        // Call the event, if cancelled don't send Action Bar
+        ActionBarMessageEvent actionBarMessageEvent = new ActionBarMessageEvent(player, message);
+        Bukkit.getPluginManager().callEvent(actionBarMessageEvent);
+        if (actionBarMessageEvent.isCancelled())
+            return;
 
-		try {
-			Class<?> c1 = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
-			Object p = c1.cast(player);
-			Object ppoc;
-			Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
-			Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + ".Packet");
-			if (useOldMethods) {
-				Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatSerializer");
-				Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-				Method m3 = c2.getDeclaredMethod("a", String.class);
-				Object cbc = c3.cast(m3.invoke(c2, "{\"text\": \"" + message + "\"}"));
-				ppoc = c4.getConstructor(new Class<?>[]{c3, byte.class}).newInstance(cbc, (byte) 2);
-			} else {
-				Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
-				Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-				Object o = c2.getConstructor(new Class<?>[]{String.class}).newInstance(message);
-				ppoc = c4.getConstructor(new Class<?>[]{c3, byte.class}).newInstance(o, (byte) 2);
-			}
-			Method m1 = c1.getDeclaredMethod("getHandle");
-			Object h = m1.invoke(p);
-			Field f1 = h.getClass().getDeclaredField("playerConnection");
-			Object pc = f1.get(h);
-			Method m5 = pc.getClass().getDeclaredMethod("sendPacket", c5);
-			m5.invoke(pc, ppoc);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			works = false;
-		}
-	}
+        if (nmsver.startsWith("v1_12_")) {
+            sendActionBarPost112(player, message);
+        } else {
+            sendActionBarPre112(player, message);
+        }
+    }
 
-	public static void sendActionBar(final Player player, final String message, int duration) {
-		sendActionBar(player, message);
+    private static void sendActionBarPost112(Player player, String message) {
+        if (!player.isOnline()) {
+            return; // Player may have logged out
+        }
 
-		if (duration >= 0) {
-			// Sends empty message at the end of the duration. Allows messages shorter than 3 seconds, ensures precision.
-			new BukkitRunnable() {
-				@Override
-				public void run() {
-					sendActionBar(player, "");
-				}
-			}.runTaskLater(plugin, duration + 1);
-		}
+        try {
+            Class<?> craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
+            Object craftPlayer = craftPlayerClass.cast(player);
+            Object ppoc;
+            Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
+            Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + ".Packet");
+            Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
+            Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
+            Class<?> chatMessageTypeClass = Class.forName("net.minecraft.server." + nmsver + ".ChatMessageType");
+            Object[] chatMessageTypes = chatMessageTypeClass.getEnumConstants();
+            Object chatMessageType = null;
+            for (Object obj : chatMessageTypes) {
+                if (obj.toString().equals("GAME_INFO")) {
+                    chatMessageType = obj;
+                }
+            }
+            Object o = c2.getConstructor(new Class<?>[]{String.class}).newInstance(message);
+            ppoc = c4.getConstructor(new Class<?>[]{c3, chatMessageTypeClass}).newInstance(o, chatMessageType);
+            Method m1 = craftPlayerClass.getDeclaredMethod("getHandle");
+            Object h = m1.invoke(craftPlayer);
+            Field f1 = h.getClass().getDeclaredField("playerConnection");
+            Object pc = f1.get(h);
+            Method m5 = pc.getClass().getDeclaredMethod("sendPacket", c5);
+            m5.invoke(pc, ppoc);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            works = false;
+        }
+    }
 
-		// Re-sends the messages every 3 seconds so it doesn't go away from the player's screen.
-		while (duration > 60) {
-			duration -= 60;
-			int sched = duration % 60;
-			new BukkitRunnable() {
-				@Override
-				public void run() {
-					sendActionBar(player, message);
-				}
-			}.runTaskLater(plugin, (long) sched);
-		}
-	}
+    private static void sendActionBarPre112(Player player, String message) {
+        if (!player.isOnline()) {
+            return; // Player may have logged out
+        }
 
-	public static void sendActionBarToAllPlayers(String message) {
-		sendActionBarToAllPlayers(message, -1);
-	}
+        try {
+            Class<?> craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
+            Object craftPlayer = craftPlayerClass.cast(player);
+            Object ppoc;
+            Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
+            Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + ".Packet");
+            if (useOldMethods) {
+                Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatSerializer");
+                Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
+                Method m3 = c2.getDeclaredMethod("a", String.class);
+                Object cbc = c3.cast(m3.invoke(c2, "{\"text\": \"" + message + "\"}"));
+                ppoc = c4.getConstructor(new Class<?>[]{c3, byte.class}).newInstance(cbc, (byte) 2);
+            } else {
+                Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
+                Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
+                Object o = c2.getConstructor(new Class<?>[]{String.class}).newInstance(message);
+                ppoc = c4.getConstructor(new Class<?>[]{c3, byte.class}).newInstance(o, (byte) 2);
+            }
+            Method m1 = craftPlayerClass.getDeclaredMethod("getHandle");
+            Object h = m1.invoke(craftPlayer);
+            Field f1 = h.getClass().getDeclaredField("playerConnection");
+            Object pc = f1.get(h);
+            Method m5 = pc.getClass().getDeclaredMethod("sendPacket", c5);
+            m5.invoke(pc, ppoc);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            works = false;
+        }
+    }
 
-	public static void sendActionBarToAllPlayers(String message, int duration) {
-		for (Player p : Bukkit.getOnlinePlayers()) {
-			sendActionBar(p, message, duration);
-		}
-	}
+    public static void sendActionBar(final Player player, final String message, int duration) {
+        sendActionBar(player, message);
+
+        if (duration >= 0) {
+            // Sends empty message at the end of the duration. Allows messages shorter than 3 seconds, ensures precision.
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    sendActionBar(player, "");
+                }
+            }.runTaskLater(plugin, duration + 1);
+        }
+
+        // Re-sends the messages every 3 seconds so it doesn't go away from the player's screen.
+        while (duration > 40) {
+            duration -= 40;
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    sendActionBar(player, message);
+                }
+            }.runTaskLater(plugin, (long) duration);
+        }
+    }
+
+    public static void sendActionBarToAllPlayers(String message) {
+        sendActionBarToAllPlayers(message, -1);
+    }
+
+    public static void sendActionBarToAllPlayers(String message, int duration) {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            sendActionBar(p, message, duration);
+        }
+    }
 }
